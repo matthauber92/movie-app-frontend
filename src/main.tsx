2be21419464edaff08fedc72e71a9d887b7a5383
Matt Hauber <matthauber92@gmail.com>
2025-12-22 21:47:26 -0500

    Adding analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from '@store';
 import { ThemeProvider } from '@mui/material';
 import { getAppTheme } from './theme.ts';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <Analytics />
         <Provider store={store}>
             <ThemeProvider theme={getAppTheme('dark')}>
+                <Analytics />
                 {/*<PersistGate persistor={persist} loading={null}>*/}
                 <App />
                 {/*</PersistGate>*/}
